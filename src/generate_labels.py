c761fd4d0b97f67afb41278faf4a4f411ae368d1
cyrozap <cyrozap@gmail.com>
2023-03-23 23:39:14 -0500

    Generate labels for global variables

--- src/generate_labels.py
+++ src/generate_labels.py
@@ -3,7 +3,7 @@
 
 # generate_labels.py - A tool to generate a list of labels for Ghidra from an
 # 8051 assembler code listing.
-# Copyright (C) 2022  Forest Crossman <cyrozap@gmail.com>
+# Copyright (C) 2022-2023  Forest Crossman <cyrozap@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,7 +24,14 @@ import re
 import sys
 
 
-REGEX = re.compile(r'^\s+([0-9A-F]+)\s+[0-9]+\s([a-zA-Z0-9_]+):$')
+VARS_REGEX = re.compile(r'^\s+[0-9]+\s+; Global variables$')
+HOME_REGEX = re.compile(r'^\s+[0-9]+\s+\.area\s+HOME\s+\([A-Z,]+\)$')
+IRAM_REGEX = re.compile(r'^\s+([0-9A-F]+)\s+[0-9]+\s([a-zA-Z0-9_]+)\s+=\s+.+$')
+CODE_REGEX = re.compile(r'^\s+([0-9A-F]+)\s+[0-9]+\s([a-zA-Z0-9_]+):$')
+
+STATE_IDLE = 0
+STATE_IRAM = 1
+STATE_CODE = 2
 
 
 def main():
@@ -39,19 +46,40 @@ def main():
     if args.output:
         output = open(args.output, 'w')
 
+    state = STATE_IDLE
     for line in listing:
         line = line.strip('\n')
-        match = REGEX.fullmatch(line)
-        if not match:
-            continue
 
-        addr, symbol = match.groups()
-        addr = int(addr, 16)
+        if state == STATE_IDLE:
+            if VARS_REGEX.fullmatch(line):
+                state = STATE_IRAM
 
-        addr_string = "CODE:0x{:04X}".format(addr)
+        elif state == STATE_IRAM:
+            iram_match = IRAM_REGEX.fullmatch(line)
+            if iram_match:
+                addr, symbol = iram_match.groups()
+                addr = int(addr, 16)
 
-        label = "{} {} l\n".format(symbol, addr_string)
-        output.write(label)
+                addr_string = "INTMEM:0x{:02X}".format(addr)
+                if symbol.startswith("misc_bit"):
+                    addr_string = "BITS:0x{:02X}".format(addr)
+
+                label = "{} {} l\n".format(symbol, addr_string)
+                output.write(label)
+
+            elif HOME_REGEX.fullmatch(line):
+                state = STATE_CODE
+
+        elif state == STATE_CODE:
+            code_match = CODE_REGEX.fullmatch(line)
+            if code_match:
+                addr, symbol = code_match.groups()
+                addr = int(addr, 16)
+
+                addr_string = "CODE:0x{:04X}".format(addr)
+
+                label = "{} {} l\n".format(symbol, addr_string)
+                output.write(label)
 
     output.close()
 
